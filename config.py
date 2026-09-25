2585cc692db85c2df19eebbaba7a20d8c7be2773
Jaideep-7-jai <92253961+Jaideep-7-jai@users.noreply.github.com>
2026-09-25 18:35:01 +0530

    Simplify email recipient lists in config.py
    Removed duplicate email addresses from CPAUSER_EMAIL and TECH_NOTIFICATION_RECIPIENTS.

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ AWS_SECRET_KEY = ''
 
 # Email config
 SENDER = 'datateam@zds-tech-prod-api-01.bo3.e-dialog.com'
-CPAUSER_EMAIL = ["cpateam@aptroid.com","datateam@aptroid.com"]
-TECH_NOTIFICATION_RECIPIENTS = ["jmolugu@zetaglobal.com","datateam@aptroid.com"]
+CPAUSER_EMAIL = ["cpateam@aptroid.com"]
+TECH_NOTIFICATION_RECIPIENTS = ["jmolugu@zetaglobal.com"]
 
 DB_HOST = 'zds-prod-pgdb02-01.bo3.e-dialog.com'
 S3_BASE = 's3://temporary-data/DATATEAM_DND_FILES/CPA_SUPP_MALNG_REQ'
